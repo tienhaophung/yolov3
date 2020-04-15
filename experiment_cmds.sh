36449c2b269db881be18903621e71ea41c728c01
tienhaophung <tienhaophung@gmail.com>
2020-04-15 20:02:32 +0700

    Change cmd

--- experiment_cmds.sh
+++ experiment_cmds.sh
@@ -3,4 +3,4 @@ python train.py --cfg cfg/yolov3-spp-custom.cfg --data data/custom/custom.data -
 # Testing
 python test.py --cfg cfg/yolov3-spp-custom.cfg --data data/custom/custom.data --weights weights/best.pt --single-cls --batch-size 8 --setname test
 # Detect
-python detect.py --cfg cfg\yolov3-spp-custom.cfg --names data/custom/classes.names --weights weights/best.pt --source C:\Users\User\Desktop\simple-HRNet\hand_images --img-size 416
+python detect.py --cfg cfg\yolov3-spp-custom.cfg --names data/custom/classes.names --weights weights/yolov3-best-100eps-1e-3-adam.pt --source C:\Users\User\Desktop\simple-HRNet\hand_images --img-size 640 --conf-thres 0.35 --iou-thres 0.15
